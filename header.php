358acd15454fd1d6cd3d9d0b53aeab7607548bed
Andrew Bielecki <ambielecki@gmail.com>
2016-03-23 09:35:30 -0400

    moved to scss

--- header.php
+++ header.php
@@ -1,5 +1,45 @@
 <header>
     <div class="container-fluid">
-        <div class="jumbotron" id="site_header"><h1>Biking on the North Shore of Boston</h1></div>
+        <div class="jumbotron bg-gradient" id="site_header"><h1>Biking on the North Shore of Boston</h1></div>
     </div>
 </header>
+<!-- Bootstrap nav -->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#llnavbar" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Home</a>
+        </div><!-- end no collapse area -->
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="llnavbar">
+            <!-- collapsable area -->
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mountain Biking <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Middlesex Fells</a></li>
+                        <li><a href="#">Lynn Woods</a></li>
+                        <li><a href="#">Willowdale</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Road Biking</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Events <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Planned Events</a></li>
+                        <li><a href="#">Meetup / Social</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Login / Signup</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <title>Biking Around the North Shore</title>
     <!-- Latest compiled and minified CSS -->
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
-    <!-- Latest compiled and minified JavaScript -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/custom.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/site.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
