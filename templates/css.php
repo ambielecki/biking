2bb7bdb4380bbca48d3c1d2709f0e501572198d2
Andrew Bielecki <ambielecki@gmail.com>
2016-03-26 08:50:04 -0400

    templating work

--- templates/css.php
+++ templates/css.php
@@ -1,4 +1,4 @@
 
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="css/site.css">
+<link rel="stylesheet" type="text/css" href="/css/site.css">
