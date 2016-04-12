2e6cff7d59a98fd10e1276b21511ca7fc1102030
Andrew Bielecki <ambielecki@gmail.com>
2016-04-12 11:08:03 -0400

    map work

--- public/js/fells.js
+++ public/js/fells.js
@@ -6,4 +6,9 @@ function initMap() {
         center: {lat: 42.451911, lng: -71.101838},
         zoom: 13
     });
+    var sheepsFold = new google.maps.Marker({
+        position: {lat: 42.4515753, lng: -71.1048641},
+        map: fells,
+        title: 'Sheepsfold Entrance'
+    });
 }
