3df32ede1999c5d02d0f4e2f93faa2274350e7f7
Andrew Bielecki <ambielecki@gmail.com>
2016-04-12 11:01:00 -0400

    map work

--- public/js/fells.js
+++ public/js/fells.js
@@ -3,7 +3,7 @@
  */
 function initMap() {
     var fells = new google.maps.Map(document.getElementById('fells_map'), {
-        center: {lat: 42.429046, lng: -71.052139},
-        zoom: 15
+        center: {lat: 42.451911, lng: -71.101495},
+        zoom: 14
     });
 }
