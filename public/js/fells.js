5518abf3e7b0055dfdf1f6f1c405d2c783e771cb
Andrew Bielecki <ambielecki@gmail.com>
2016-04-12 10:41:10 -0400

    map work

--- public/js/fells.js
+++ public/js/fells.js
@@ -2,7 +2,7 @@
  * Created by Bielecki on 4/12/2016.
  */
 function initMap() {
-    var kierstead = new google.maps.Map(document.getElementById('fells_map'), {
+    var fells = new google.maps.Map(document.getElementById('fells_map'), {
         center: {lat: 42.429046, lng: -71.052139},
         zoom: 15
     });
