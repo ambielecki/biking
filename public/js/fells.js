7121ce44e28a93e99b08da162dffc343feade455
Andrew Bielecki <ambielecki@gmail.com>
2016-04-28 13:43:25 -0400

    added images

--- public/js/fells.js
+++ public/js/fells.js
@@ -1,19 +1,19 @@
 /**
  * Created by Bielecki on 4/12/2016.
  */
-$(document).ready(function() {
-    function initMap() {
-        var fells = new google.maps.Map(document.getElementById('fells_map'), {
-            center: {lat: 42.451911, lng: -71.101838},
-            zoom: 13
-        });
-        var sheepsFold = new google.maps.Marker({
-            position: {lat: 42.4515753, lng: -71.1048641},
-            map: fells,
-            title: 'Sheepsfold Entrance'
-        });
-    }
+function initMap() {
+    var fells = new google.maps.Map(document.getElementById('fells_map'), {
+        center: {lat: 42.451911, lng: -71.101838},
+        zoom: 13
+    });
+    var sheepsFold = new google.maps.Marker({
+        position: {lat: 42.4515753, lng: -71.1048641},
+        map: fells,
+        title: 'Sheepsfold Entrance'
+    });
+}
 
+$(document).ready(function() {
     $(".fancybox").fancybox({
         openEffect	: 'none',
         closeEffect	: 'none'
